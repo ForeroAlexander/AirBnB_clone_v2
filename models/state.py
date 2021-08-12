39a7f85f5f53972fd3f7633387897aee275db123
ForeroAlexander <foreroalex26@gmail.com>
2021-08-11 21:03:04 -0500

    gohan eres la unica salvacion

--- models/state.py
+++ models/state.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from os import getenv
-from sqlalchemy.orm import relationship
-from sqlalchemy.sql.schema import Column
-from sqlalchemy.sql.sqltypes import String
-from models.base_model import Base, BaseModel
+import os
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship, backref
+from models.city import City
+import models
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
 
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        name = Column(String(128), nullable=False)
-    else:
-        name = ""
-
+    if os.getenv('HBNB_TYPE_STORAGE') == 'fs':
         @property
         def cities(self):
-            """returns the list of City instances with state_id"""
-            from models import storage
-            from models.city import City
-            items = storage.all(City).items()
-            result = []
-            for _, value in items:
-                if value.state_id == self.id:
-                    result.append(value)
-            return (result)
+            _cities = []
+            for _id, city in models.storage.all(City).items():
+                if self.id == city.state_id:
+                    _cities.append(city)
+            return _cities
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City",
+                              backref="state",
+                              cascade="all, delete")
