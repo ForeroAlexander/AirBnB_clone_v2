5298084bc5377c6c069c7f4f8cac8e2e5c10afca
Edheramirez <edher.ramirez@outlook.es>
2021-08-12 00:16:59 -0500

    fix identation

--- models/place.py
+++ models/place.py
@@ -21,29 +21,67 @@ place_amenity = Table('place_amenity', metadata,
 class Place(BaseModel, Base):
     """This class define a table with various atributtes"""
     __tablename__ = "places"
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, nullable=False, default=0)
-    number_bathrooms = Column(Integer, nullable=False, default=0)
-    max_guest = Column(Integer, nullable=False, default=0)
-    price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullable=True)
-    longitude = Column(Float, nullable=True)
-    amenity_ids = []
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=True)
+        number_rooms = Column(Integer, nullable=False, default=0)
+        number_bathrooms = Column(Integer, nullable=False, default=0)
+        max_guest = Column(Integer, nullable=False, default=0)
+        price_by_night = Column(Integer, nullable=False, default=0)
+        latitude = Column(Float, nullable=True)
+        longitude = Column(Float, nullable=True)
+        amenity_ids = []
 
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    else:
+        else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
+
+        @property
+        def reviews(self):
+            """getter attribute returns the list of Review instances"""
+            review_list = []
+            all_reviews = models.storage.all(Review)
+            for review in all_reviews.values():
+                if review.place_id == self.id:
+                    review_list.append(review)
+            return review_list
+
+        @property
+        def amenities(self):
+            """getter attribute returns the list of Amenity instances"""
+            return self.amenity_ids
+
+        @amenities.setter
+        def amenities(self, amenity):
+            """setter amenities ids to Amenity instances"""
+            if self.__class__.__name__ == 'Amenity' and amenity.id\
+               not in self.amenity_ids:
+                self.amenity_ids.append(amenity.id)
+
+
+
+    """if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         reviews = relationship("Review", backref="place",
                                cascade="all, delete")
         amenities = relationship('Amenity',
                                  secondary='place_amenity',
                                  viewonly=False)
-
     else:
         @property
         def reviews(self):
-            """getter method place"""
+getter method place
             _review = []
             for _id, obj in models.storage.all(Review).items():
                 if self.id == obj.place_id:
@@ -52,7 +90,7 @@ class Place(BaseModel, Base):
 
         @property
         def kamenities(self):
-            """getter method amenities"""
+getter method amenities
             _amenities = []
             for _id, obj in models.storage.all(Amenity).items():
                 if self.id == obj.place_id:
@@ -61,6 +99,7 @@ class Place(BaseModel, Base):
 
         @amenities.setter
         def amenities(self, value):
-            """setter method"""
+setter method
             if type(value) is 'Amenity':
                 self.amenity_ids.append(value.id)
+"""
