d71af920e6d2102b068c63250b5a576447a5bad4
Edheramirez <edher.ramirez@outlook.es>
2021-08-12 00:58:26 -0500

    fix identation

--- models/city.py
+++ models/city.py
@@ -3,12 +3,18 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship, backref
+from os import getenv
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
-    name = Column("name", String(128), nullable=False)
-    state_id = Column("state_id", String(60), ForeignKey("states.id"),
-                      nullable=False)
-    places = relationship("Place", backref="cities", cascade="all, delete")
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey("states.id"),
+                          nullable=False)
+        places = relationship("Place", backref="cities", cascade="all, delete")
+
+    else:
+        state_id = ""
+        name = ""
