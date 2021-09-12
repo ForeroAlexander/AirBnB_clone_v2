523865b7a873069f6209fd55dcda1fb7ef2818a4
ForeroAlexander <foreroalex26@gmail.com>
2021-09-12 02:37:09 -0500

    HBNB!!

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Start Flask """
+from flask import Flask, app
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Hello HBNB! """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    """HBNB!!"""
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run()
