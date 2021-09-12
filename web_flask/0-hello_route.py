86ae89b4db5ce2977ea3aa4472a9f970fe2b92f8
ForeroAlexander <foreroalex26@gmail.com>
2021-09-12 01:50:13 -0500

    Hello Flask!

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Start Flask """
+from flask import Flask, app
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    app.run()
