bfd89960798f6f419717e2bfcca0a983faf2f860
ForeroAlexander <foreroalex26@gmail.com>
2021-09-12 23:54:09 -0500

    fix

--- models/state.py
+++ models/state.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-import os
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship, backref
-from models.city import City
+"""This is the state class"""
+from os import getenv
+from sqlalchemy import String, DateTime, Column, ForeignKey
+from sqlalchemy.orm import relationship
 import models
+from models.base_model import BaseModel, Base
+from models.city import City
 
 
 class State(BaseModel, Base):
-    """ State class """
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete")
 
-    if os.getenv('HBNB_TYPE_STORAGE') == 'fs':
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', backref='state',
+                              cascade='all, delete-orphan')
+    else:
         @property
         def cities(self):
-            _cities = []
-            for _id, city in models.storage.all(City).items():
-                if self.id == city.state_id:
-                    _cities.append(city)
-            return _cities
-
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship("City",
-                              backref="state",
-                              cascade="all, delete")
+            """Getter attribute in case of file storage"""
+            return [city for city in models.storage.all(City).values()
+                    if city.state_id == self.id]
